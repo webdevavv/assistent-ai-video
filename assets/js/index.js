a9fca3e2b65526d63eb212ddb4732d766c8c6037
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2024-11-14 15:45:20 +0400

    finish

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,35 +24,59 @@ document.querySelectorAll('.often-asks-item').forEach(item => {
     });
 });
 
-const playButton = document.getElementById("playButton");
-const videoContainer = document.getElementById("videoContainer");
-const videoPlayer = document.getElementById("videoPlayer");
 
-let isPlaying = false;
 
-function toggleVideo() {
-    if (!isPlaying) {
-        videoContainer.style.backgroundImage = "none";
-        videoPlayer.style.display = "block";
-        videoPlayer.play();
-        playButton.style.display = "none";
-        isPlaying = true;
-    } else {
-        videoContainer.style.backgroundImage = "url('assets/img/convertimage.webp')";
+
+function updateSlider(slider, valueDisplay) {
+    const tempSliderValue = slider.value;
+    valueDisplay.textContent = tempSliderValue;
+
+    const progress = (tempSliderValue / slider.max) * 100;
+
+    slider.style.background = `linear-gradient(to right, #0057FF ${progress}%, #ccc ${progress}%)`;
+}
+
+document.querySelectorAll('.range-input').forEach(sliderEl => {
+    const valueDisplay = document.querySelector(sliderEl.getAttribute('data-value'));
+
+    sliderEl.addEventListener('input', () => updateSlider(sliderEl, valueDisplay));
+
+    updateSlider(sliderEl, valueDisplay);
+})
+
+
+
+
+document.querySelectorAll('.video-block').forEach(block => {
+    const playButton = block.querySelector('.playButton');
+    const videoPlayer = block.querySelector('.videoPlayer');
+    const defaultImage = block.getAttribute('data-image');
+    let isPlaying = false;
+
+    function toggleVideo() {
+        if (!isPlaying) {
+            block.style.backgroundImage = "none";
+            videoPlayer.style.display = "block";
+            videoPlayer.play();
+            playButton.style.display = "none";
+            isPlaying = true;
+        } else {
+            block.style.backgroundImage = `url('${defaultImage}')`;
+            videoPlayer.style.display = "none";
+            videoPlayer.pause();
+            videoPlayer.currentTime = 0;
+            playButton.style.display = "block";
+            isPlaying = false;
+        }
+    }
+
+    playButton.addEventListener("click", toggleVideo);
+
+    videoPlayer.addEventListener("ended", function() {
+        block.style.backgroundImage = `url('${defaultImage}')`;
         videoPlayer.style.display = "none";
-        videoPlayer.pause();
         videoPlayer.currentTime = 0;
         playButton.style.display = "block";
         isPlaying = false;
-    }
-}
-
-playButton.addEventListener("click", toggleVideo);
-
-videoPlayer.addEventListener("ended", function() {
-    videoContainer.style.backgroundImage = "url('assets/img/convertimage.webp')";
-    videoPlayer.style.display = "none";
-    videoPlayer.currentTime = 0;
-    playButton.style.display = "block";
-    isPlaying = false;
+    });
 });
